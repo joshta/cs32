d416b62a270be73e45a64b3e87183818b9bcf980
joshta <joshta>
2016-01-22 14:04:01 -0800

    valgrind

---
+++ discussion3/valgrind.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+valgrind --verbose --tool=memcheck --show-reachable=yes --track-fds=yes --trace-children=yes \
+            --track-origins=yes --leak-check=full --log-file=valgrind.log ./a.out
+
